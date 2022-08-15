e1f761cdfb24edddaed1370b0d8c43e34e9667ad
Nicole J. C. Lemes <nicolejdecarvalho@gmail.com>
2022-08-15 15:45:06 -0300

    fix: deploy build issues

--- dist/src/routers/disciplineRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getDisciplines, postDiscipline } from "../controllers/disciplineController.js";
-import { authentication } from "../middlewares/authentication.js";
-import schemaValidation from "../middlewares/schemaValidation.js";
-import { disciplineSchema } from "../schemas/planningSchema.js";
-var disciplineRouter = Router();
-disciplineRouter.post("/disciplines", authentication, schemaValidation(disciplineSchema), postDiscipline);
-disciplineRouter.get("/disciplines", authentication, getDisciplines);
-export default disciplineRouter;
--- dist/src/server.js
+++
@@ -1,4 +0,0 @@
-import app from "../src/config/app.js";
-import "../src/config/setup.js";
-var PORT = Number(process.env.PORT) || 5000;
-app.listen(PORT, function () { return console.log("The server is running on port " + PORT); });
